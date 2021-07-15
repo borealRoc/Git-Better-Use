5815b055fa5b153ad900670a391a8ba05e9cc8b0
xusp <xspstudyemail@163.com>
2021-07-15 10:19:55 +0800

    第二次 stash

--- stash/index.js
+++ stash/index.js
@@ -1,1 +1,11 @@
 console.log('测试 git stash svae messange ~~')
+
+console.log(`
+刚才我切换到 cherry-pick-1 分支，
+所以 stash 了一些 change, 
+现在我 checkout 回来, 
+但还没 stash pop 刚才的 stash,
+就新增了这条信息, 
+现在我又临时有事再切换到 cherry-pick-1 分支,
+再 stash 一次这条信息
+`)
